89d65a1cf92f4b18d022d46a5a2144494347b4f0
Theresa Mai <tmai7@huskers.unl.edu>
2020-08-18 02:34:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Theresa Mai
+ * Date: 8/17/2020
+ *
+ * A simple hello world program in C
+ *
+ * */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+     printf("Hello World!\n");
+
+     return 0;
+ }
